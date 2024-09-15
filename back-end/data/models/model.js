aa7f7636b86f7f7e074f5787da8a587a4a379434
drakenevadie19 <drakennievadje19@gmail.com>
2024-09-15 04:22:55 -0500

    new Change: merge data from 2 files and push to MongoDB

--- back-end/data/models/model.js
+++ back-end/data/models/model.js
@@ -3,11 +3,30 @@ const mongoose = require('mongoose');
 // Schema = a predefined format or a pattern just like we have with the queries
 const mongoSchema = new mongoose.Schema({ 
     // Arguments are data that we want to provide to store in the schema
-    name: String, 
-    genre: String, 
-    year: String
+    title: String,
+    year: String,
+    rated: String,
+    released: String,
+    runtime: String,
+    genre: String,
+    director: String,
+    writer: String,
+    actors: String,
+    plot: String,
+    language: String,
+    country: String,
+    awards: String,
+    poster: String,
+    type: String,
+    images: [String],
+    rates: {
+        metaScore: String,
+        imdbRating: String,
+        imdbVotes: String,
+        // rottenTomatoes: null,
+    }
 });
 
 // Create a model with schema we provided with name 'MongoMovieModel'
 //  => Everything upload to DB will follow this schema
-module.exports = new mongoose.model('MongoMovieModel', mongoSchema);
+module.exports = new mongoose.model('MongoMovieListModel', mongoSchema);
--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,125 @@
+require('dotenv').config();
 const express = require('express')
-const app = express()
+const app = express();
+const fs = require('fs');
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+const mongoose = require("mongoose");
+
+const mongoModel = require('./data/models/model');
+console.log(mongoModel);
+
+// Connect BE to MongoDB Atlas DB
+// No need useNewURLParser and useUnifiedTopology parameters, because they are deprecated.
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@movie-maker-app.9l1xvlj.mongodb.net/?retryWrites=true&w=majority&appName=movie-maker-app`)
+    .then(() => {
+        console.log("Successfully connect to MongoDB");
+    })
+    .catch((err) => {
+        console.log('Error:', err);  
+    });
+
+// console.log(process.env.MONGO_USERNAME);
+// console.log(process.env.MONGO_PASSWORD);
+
+const resultList = [];
+const existedName = [];
+fs.readFile("./data/Film1.JSON", 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("reading file 1 successfully");
+    let movieList1 = JSON.parse(data);
+    movieList1.forEach((movie) => {
+
+      if (!existedName.includes(movie.Title.toLowerCase())) {
+        // console.log("Element movie here:");
+        // console.log(movie.Title);
+        resultList.push(
+          {
+            title: movie.Title,
+            year: movie.Year.toString(),
+            rated: movie.Rated,
+            released: movie.Released,
+            runtime: movie.Runtime,
+            genre: movie.Genre,
+            director: movie.Director,
+            writer: movie.Writer,
+            actors: movie.Actors,
+            plot: movie.Plot,
+            language: movie.Language,
+            country: movie.Country,
+            awards: movie.Awards,
+            poster: movie.Poster,
+            type: movie.Type,
+            images: movie.Images,
+            rates: {
+              metaScore: movie.Metascore.toString(),
+              imdbRating: movie.imdbRating.toString(),
+              imdbVotes: movie.imdbVotes,
+              // rottenTomatoes: null,
+            }
+          }
+        );
+
+        existedName.push(movie.Title.toLowerCase());
+      }
+      
+    });
+  }
+});
+
+
+fs.readFile("./data/filmList.json", 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("reading file 1 successfully");
+    let movieList1 = JSON.parse(data);
+    movieList1.forEach((movie) => {
+      if (!existedName.includes(movie.Title.toLowerCase())) {
+        // console.log("Element movie here:");
+        // console.log(movie.Title);
+        resultList.push(
+          {
+            title: movie.Title,
+            year: movie.Year.toString(),
+            rated: movie.Rated,
+            released: movie.Released,
+            runtime: movie.Runtime,
+            genre: movie.Genre,
+            director: movie.Director,
+            writer: movie.Writer,
+            actors: movie.Actors,
+            plot: movie.Plot,
+            language: movie.Language,
+            country: movie.Country,
+            awards: movie.Awards,
+            poster: movie.Poster,
+            type: movie.Type,
+            images: movie.Images,
+            rates: {
+              metaScore: movie.Metascore.toString(),
+              imdbRating: movie.imdbRating.toString(),
+              imdbVotes: movie.imdbVotes,
+              // rottenTomatoes: null,
+            }
+          }
+        );
+
+        existedName.push(movie.Title.toLowerCase());
+      }
+    });
+  }
+});
+
+
+app.get('/update', function (req, res) {
+  // console.log();
+  res.send(resultList);
+  resultList.map((movie) => {
+    let newMovie = new mongoModel(movie);
+    newMovie.save();
+  })
 })
 
 app.listen(8001, () => {
